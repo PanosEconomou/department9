7f399404940b6a69ee6b73a4f31d18f8e055f220
Panos Oikonomou <po524@nyu.edu>
2025-07-11 23:05:35 -0400

    Buttons in UI

---
+++ core/ui/terminal.py
@@ -0,0 +1,35 @@
+from textual.app import App, ComposeResult
+from textual.containers import Horizontal, VerticalScroll
+from textual.widgets import Header, Footer, Button, Static
+
+
+class Terminal(App):
+
+    CSS_PATH = "terminal.tcss"
+    BINDINGS = [("d", "toggle_dark" ,"Toggle Dark Mode")]
+
+    def compose(self) -> ComposeResult:
+        yield Header()
+        yield Footer()
+        yield Horizontal(
+            VerticalScroll(
+                Static("Menu", classes="header"),
+                Button("Information"),
+                Button("Documents"),
+                Button("Email"),
+                Button("Secure Messaging")
+            )
+        )
+
+    def on_mount(self) -> None:
+        self.title      = "Depratment 9"
+        self.sub_title  = "Remote Access Console"
+
+    def action_toggle_dark(self) -> None:
+        self.theme = (
+            "textual-dark" if self.theme == "textual-light" else "textual-light"
+        )
+
+if __name__ == "__main__":
+    app = Terminal()
+    app.run()
