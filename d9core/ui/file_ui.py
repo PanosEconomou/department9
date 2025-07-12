4ae98d66202e8957ffdac8b187c63b3ddd6b9d11
Panos Oikonomou <panos.economou.v@gmail.com>
2025-07-12 17:13:27 -0400

    document view mvp

--- d9core/ui/file_ui.py
+++ d9core/ui/file_ui.py
@@ -1,17 +1,23 @@
+from textual.containers import Container
+from textual.app import ComposeResult
+from textual.widgets import Markdown, TabbedContent, TabPane
+
 from d9core.engine.file_manager import get_documents
-from textual.app import App, ComposeResult
-from textual.widgets import Label, Markdown, TabbedContent, TabPane
 
-class DocumentView(App):
-
-    data = get_documents()
+class DocumentView(Container):
 
     def compose(self) -> ComposeResult:
-        with TabbedContent():
-            for document in self.data:
+
+        data = get_documents()
+
+        if not data:
+            yield Markdown("No documents yet.")
+            return
+        
+        with TabbedContent(initial=data[0]["id"]):
+            for document in data:
                 with TabPane(document["id"],id=document["id"]):
                     yield Markdown(document["content"])
 
-
-if __name__ == "__main__":
-    DocumentView().run()
+    def action_show_tab(self, tab: str) -> None:
+        self.get_child_by_type(TabbedContent).active = tab
--- d9core/ui/terminal.py
+++ d9core/ui/terminal.py
@@ -1,6 +1,8 @@
 from textual.app import App, ComposeResult
 from textual.containers import Horizontal, VerticalScroll
-from textual.widgets import Header, Footer, Button, Static
+from textual.widgets import Header, Footer, Button, Static, ContentSwitcher
+
+from d9core.ui.file_ui import DocumentView
 
 
 class Terminal(App):
@@ -11,19 +13,24 @@ class Terminal(App):
     def compose(self) -> ComposeResult:
         yield Header()
         yield Footer()
-        yield Horizontal(
-            VerticalScroll(
-                Static("Menu", classes="header"),
-                Button("Information"),
-                Button("Documents"),
-                Button("Email"),
-                Button("Secure Messaging")
-            )
-        )
+        with Horizontal(id="main-view"):
+            with VerticalScroll(id="buttons"):
+                yield Button("Information", id="information-view")
+                yield Button("Documents", id="document-view")
+                yield Button("Email", id="email-view")
+                yield Button("Secure Messaging", id="messaging-view")
+            with ContentSwitcher(initial="information-view", id="content-view"):
+                yield Static("Information View Coming Soon...", id="information-view")
+                yield DocumentView(id="document-view")
+                yield Static("Email View Coming Soon...", id="email-view")
+                yield Static("Secure Messaging View Coming Soon...", id="messaging-view")
 
     def on_mount(self) -> None:
         self.title      = "Depratment 9"
         self.sub_title  = "Remote Access Console"
+
+    def on_button_pressed(self, event:Button.Pressed) -> None:
+        self.query_one(ContentSwitcher).current = event.button.id
 
     def action_toggle_dark(self) -> None:
         self.theme = (
